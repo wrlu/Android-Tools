486cd5ef8a0e2c62c2e8212169ac2f1a9ffee1df
wrlu <wrlu.cn@outlook.com>
2025-03-24 10:49:44 +0800

    Fix is_permission_privileged & add path permission support.

--- on_filesystem/vulscan/search_permission.py
+++ on_filesystem/vulscan/search_permission.py
@@ -36,8 +36,16 @@ def get_defined_permissions(manifest):
         permissions_name_level.append({'name': name, 'protectionLevel': protection_level})
     return permissions_name_level
 
+# https://developer.android.com/reference/android/R.attr#protectionLevel
 def is_permission_privileged(defined_permission):
-    return not ('normal' in defined_permission['protectionLevel'] or 'dangerous' in defined_permission['protectionLevel'])
+    if defined_permission['protectionLevel'] == '':
+        return False
+    protectionLevelInt = int(defined_permission['protectionLevel'], base=16)
+    if protectionLevelInt | 1000 == 1:
+        return False
+    if protectionLevelInt | 1 == 1:
+        return False
+    return True
 
 def get_uses_permissions(manifest):
     uses_permissions = manifest.getElementsByTagName('uses-permission')
@@ -100,6 +108,18 @@ def get_provider_read_permission(provider):
 def get_provider_write_permission(provider):
     return provider.getAttribute('android:writePermission')
 
+def get_provider_path(provider):
+    return provider.getAttribute('android:path')
+
+def get_provider_path_prefix(provider):
+    return provider.getAttribute('android:pathPrefix')
+
+def get_provider_path_pattern(provider):
+    return provider.getAttribute('android:pathPattern')
+
+def get_provider_path_permission(provider):
+    return provider.getElementsByTagName('path-permission')
+
 def collect_permission_info(xml_content):
     manifest_file_content = minidom.parse(xml_content)
 
@@ -147,12 +167,30 @@ def collect_permission_info(xml_content):
             permission = get_componment_permission(provider)
             read_permission = get_provider_read_permission(provider)
             write_permission = get_provider_write_permission(provider)
+            path_permission = []
+            for path_permission_element in get_provider_path_permission(provider):
+                path = get_provider_path(path_permission_element)
+                path_prefix = get_provider_path_prefix(path_permission_element)
+                path_pattern = get_provider_path_pattern(path_permission_element)
+                path_permission_def = get_componment_permission(path_permission_element)
+                path_read_permission = get_provider_read_permission(path_permission_element)
+                path_write_permission = get_provider_write_permission(path_permission_element)
+                path_permission.append({
+                    'path': path,
+                    'pathPrefix': path_prefix,
+                    'pathPattern': path_pattern,
+                    'permission': path_permission_def,
+                    'readPermission': path_read_permission,
+                    'writePermission': path_write_permission
+                })
+                
             componments.append({
                 'name': full_componment_name,
                 'type': 'provider',
                 'permission': permission,
                 'readPermission': read_permission,
-                'writePermission': write_permission
+                'writePermission': write_permission,
+                'path_permission': path_permission
             })
     
     for receiver in receivers:
@@ -172,7 +210,7 @@ def collect_permission_info(xml_content):
         'uses_permissions': uses_permissions,
         'protected_broadcasts': protected_broadcasts
     }
-    return result
+    return package_name, result
 
 def search_componment_permission_issues(base_data):
     undef_pem_comps = {
@@ -187,7 +225,19 @@ def search_componment_permission_issues(base_data):
         'provider': [],
         'receiver': []
     }
-    for componment in base_data['componments']:
+    base_data_merge = {
+        'componments': [],
+        'defined_permissions': [],
+        'uses_permissions': [],
+        'protected_broadcasts': []
+    }
+    for per_apk_data in base_data:
+        base_data_merge['componments'].extend(per_apk_data['componments'])
+        base_data_merge['defined_permissions'].extend(per_apk_data['defined_permissions'])
+        base_data_merge['uses_permissions'].extend(per_apk_data['uses_permissions'])
+        base_data_merge['protected_broadcasts'].extend(per_apk_data['protected_broadcasts'])
+    
+    for componment in base_data_merge['componments']:
         unprivileged = {
             'writePermission': True,
             'readPermission': True,
@@ -198,11 +248,12 @@ def search_componment_permission_issues(base_data):
             'readPermission': True,
             'permission': True
         }
+        # Provider has readPermission and writePermission, will use special check logic.
         if componment['type'] == 'provider':
             provider_pem_key_words = ['writePermission', 'readPermission', 'permission']
             for pem_key_word in provider_pem_key_words:
                 if pem_key_word in componment and componment[pem_key_word] != '':
-                    for defined_permission in base_data['defined_permissions']:
+                    for defined_permission in base_data_merge['defined_permissions']:
                         if defined_permission['name'] == componment[pem_key_word]:
                             undefined[pem_key_word] = False
                             if is_permission_privileged(defined_permission):
@@ -211,6 +262,22 @@ def search_componment_permission_issues(base_data):
                 else:
                     unprivileged[pem_key_word] = True
                     undefined[pem_key_word] = False
+
+            # Check path permission
+            for per_path_permission in componment['path_permission']:
+                for pem_key_word in provider_pem_key_words:
+                    if pem_key_word in per_path_permission and per_path_permission[pem_key_word] != '':
+                        for defined_permission in base_data_merge['defined_permissions']:
+                            if defined_permission['name'] == per_path_permission[pem_key_word]:
+                                undefined[pem_key_word] = False
+                                if is_permission_privileged(defined_permission):
+                                    unprivileged[pem_key_word] = False
+                                break
+                    else:
+                        unprivileged[pem_key_word] = True
+                        undefined[pem_key_word] = False
+
+
             # readPermission or writePermission undefined, and permission unprivileged or undefined.
             # the undefined readPermission/writePermission is vulnerable.
             if (unprivileged['permission'] or undefined['permission']) and (undefined['writePermission'] or undefined['readPermission']):
@@ -219,6 +286,7 @@ def search_componment_permission_issues(base_data):
                     'writePermission': componment['writePermission'],
                     'readPermission': componment['readPermission'],
                     'permission': componment['permission'],
+                    'path_permission': componment['path_permission']
                 })
             # permission undefined, and readPermission or writePermission unprivileged.
             # the undefined permission is vulnerable.
@@ -228,6 +296,7 @@ def search_componment_permission_issues(base_data):
                     'writePermission': componment['writePermission'],
                     'readPermission': componment['readPermission'],
                     'permission': componment['permission'],
+                    'path_permission': componment['path_permission']
                 })
             # permission unprivileged, and readPermission or writePermission unprivileged.
             # the unprivileged permission is vulnerable.
@@ -237,10 +306,12 @@ def search_componment_permission_issues(base_data):
                     'writePermission': componment['writePermission'],
                     'readPermission': componment['readPermission'],
                     'permission': componment['permission'],
+                    'path_permission': componment['path_permission']
                 })
+        # Other componments only have permission, use normal logic.
         else:
             if 'permission' in componment and componment['permission'] != '':
-                for defined_permission in base_data['defined_permissions']:
+                for defined_permission in base_data_merge['defined_permissions']:
                     if defined_permission['name'] == componment['permission']:
                         undefined['permission'] = False
                         if is_permission_privileged(defined_permission):
@@ -250,9 +321,10 @@ def search_componment_permission_issues(base_data):
                 unprivileged['permission'] = True
                 undefined['permission'] = False
             if componment['type'] == 'receiver':
+                # Exported receiver should have an intent filter with unprotected action if is vulnerable. 
                 has_unprotected_action = False
                 for action in componment['actions']:
-                    if action not in base_data['protected_broadcasts']:
+                    if action not in base_data_merge['protected_broadcasts']:
                         has_unprotected_action = True
                         break
                 if has_unprotected_action == False:
@@ -268,13 +340,10 @@ def search_componment_permission_issues(base_data):
                     'name': componment['name'],
                     'permission': componment['permission']
                 })
-    for comp_type in undef_pem_comps.keys():
-         with open('undef_pem_issues_' + comp_type + '.json', 'w') as f:
-            f.write(json.dumps(undef_pem_comps[comp_type]))
-    
-    for comp_type in unpriv_pem_comps.keys():
-         with open('unpriv_pem_issues_' + comp_type + '.json', 'w') as f:
-            f.write(json.dumps(unpriv_pem_comps[comp_type]))
+    final_result = {}
+    final_result['undefined_permissions'] = undef_pem_comps
+    final_result['unprivileged_permissions'] = unpriv_pem_comps
+    return final_result
 
 def process_apk(apk_file):
     print('Start analysis apk file: '+apk_file)
@@ -283,12 +352,7 @@ def process_apk(apk_file):
     return collect_permission_info(output_file)
 
 def scan_dir(packages_dir):
-    base_data = {
-        'componments': [],
-        'defined_permissions': [],
-        'uses_permissions': [],
-        'protected_broadcasts': []
-    }
+    base_data = []
 
     for package in os.listdir(packages_dir):
         if 'auto_generated_rro_product' in package:
@@ -306,28 +370,55 @@ def scan_dir(packages_dir):
                 if file.endswith('.apk'):
                     apk_file = package_dir + os.sep + file
                     if os.path.isfile(apk_file):
-                        tmp_result = process_apk(apk_file)
+                        package_name, tmp_result = process_apk(apk_file)
                         if tmp_result == None:
                             continue
-                        base_data['componments'].extend(tmp_result['componments'])
-                        base_data['defined_permissions'].extend(tmp_result['defined_permissions'])
-                        base_data['uses_permissions'].extend(tmp_result['uses_permissions'])
-                        base_data['protected_broadcasts'].extend(tmp_result['protected_broadcasts'])
+                        base_data.append({
+                            'package': package_name,
+                            'filename': apk_file,
+                            'componments': tmp_result['componments'],
+                            'defined_permissions': tmp_result['defined_permissions'],
+                            'uses_permissions': tmp_result['uses_permissions'],
+                            'protected_broadcasts': tmp_result['protected_broadcasts'],
+                        })
                         
         elif package.endswith('.apk'):
             apk_file = package_dir
             if os.path.isfile(apk_file):
-                tmp_result = process_apk(apk_file)
+                package_name, tmp_result = process_apk(apk_file)
                 if tmp_result == None:
                     continue
-                base_data['componments'].extend(tmp_result['componments'])
-                base_data['defined_permissions'].extend(tmp_result['defined_permissions'])
-                base_data['uses_permissions'].extend(tmp_result['uses_permissions'])
-                base_data['protected_broadcasts'].extend(tmp_result['protected_broadcasts'])
+                base_data.append({
+                    'package': package_name,
+                    'filename': apk_file,
+                    'componments': tmp_result['componments'],
+                    'defined_permissions': tmp_result['defined_permissions'],
+                    'uses_permissions': tmp_result['uses_permissions'],
+                    'protected_broadcasts': tmp_result['protected_broadcasts'],
+                })
+    return base_data
+
+def main():
+    if len(sys.argv) != 2:
+        print('search_permission.py: Missing parameters, usage: python search_permission.py dir')
+        sys.exit(1)
+    
+    base_data = {}
+    if os.path.exists(sys.argv[1] + os.sep + 'all_comp.json'):
+        with open(sys.argv[1] + os.sep + 'all_comp.json', 'r') as f:
+            base_data = json.load(f)
+    else:
+        base_data = scan_dir(sys.argv[1] + os.sep + 'packages')
+    
+    with open(sys.argv[1] + os.sep + 'all_comp.json', 'w') as f:
+        f.write(json.dumps(base_data))
+    
     print('Start analysis componment permissions...')
-    search_componment_permission_issues(base_data)
+    final_result = search_componment_permission_issues(base_data)
+    with open(sys.argv[1] + os.sep + 'accessible_comp.json', 'w') as f:
+        f.write(json.dumps(final_result))
+
     print('Finish!')
-if len(sys.argv) != 2:
-    print('search_permission.py: Missing parameters, usage: python search_permission.py dir')
-    sys.exit(1)
-scan_dir(sys.argv[1])
+    
+if __name__ == '__main__':
+    main()
