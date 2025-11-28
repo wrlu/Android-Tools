875f099d6bc42c814805241ef64a765fd14dcf5d
wrlu <wrlus@outlook.com>
2025-11-28 16:11:46 +0800

    Improve get_accessible_services.py

--- on_filesystem/vulscan/get_accessible_services.py
+++ on_filesystem/vulscan/get_accessible_services.py
@@ -4,42 +4,46 @@ import subprocess
 import platform
 
 def run_command(cmds, cwd='.'):
-    result = subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd, check=False)
+    result = subprocess.run(cmds, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd, text=True, check=False)
     return result.stdout
 
-def host_cmd_sesearch_check_service(source, target, tclass, perms, policy):
+def host_cmd_sesearch_svcmgr_find(source, target, policy):
     if platform.system() == 'Windows':
-        return run_command(['wsl', '-e', 'sesearch', '-A', '-s', source, '-t', target, '-c', tclass, '-p', perms, policy])
+        return run_command(['wsl', '-e', 'sesearch', '-A', '-s', source, '-t', target, '-c', 'service_manager', '-p', 'find', policy])
     else:
-        return run_command(['sesearch', '-A', '-s', source, '-t', target, '-c', tclass, policy])
+        return run_command(['sesearch', '-A', '-s', source, '-t', target, '-c', 'service_manager', '-p', 'find', policy])
 
 def untrusted_app_service_accessible_anaylsis():
-    service_names = []
+    service_tag_list = []
     with open('service_list.txt', 'r', encoding='ascii') as service_file:
-        lines = service_file.readlines()
-        for line in lines[1:]:
-            service_name = line.strip().split('\t')[1].split(':')[0]
-            service_names.append(service_name)
-    service_tag_mapping = {}
-    with open('selinux/selinux/plat_service_contexts', 'r', encoding='ascii') as context_file:
-        lines = context_file.readlines()
-        for line in lines:
-            parts = line.strip().split()
-            if len(parts) == 2:
-                name = parts[0]
-                if name in service_names:
-                    tag = parts[1].split(':')[2]
-                    service_tag_mapping[name] = tag
-    accessible_services = []
+        next(service_file)
+
+        for service_line in service_file:
+            service_name, service_aidl = service_line.strip().split('\t')[1].split(': ')
+            service_aidl = service_aidl[1:-1]
+
+            with open('selinux/selinux/plat_service_contexts', 'r', encoding='ascii') as context_file:
+                for context_line in context_file:
+                    name, label = context_line.strip().split()
+                    if name == service_name:
+                        tag = label.split(':')[2]
+                        service_tag_list.append((service_name, service_aidl, tag))
+                        break
+    
+    
+    accessible_service_tag_list = []
     i = 1
-    total = len(service_tag_mapping)
-    for service, tag in service_tag_mapping.items():
-        cmd_out = host_cmd_sesearch_check_service('untrusted_app_all', tag, 'service_manager', 'find', 'selinux/policy')
-        if b'allow' in cmd_out:
-            show_progress(i, total, f"Allow access: {service}")
-            accessible_services.append(service)
+    total = len(service_tag_list)
+    for service_name, service_aidl, tag in service_tag_list:
+        allowlist = host_cmd_sesearch_svcmgr_find('untrusted_app_all', tag, 'selinux/policy')
+
+        if 'allow' in allowlist:
+            show_progress(i, total, f"Allow access: {service_name}: [{service_aidl}]")
+            accessible_service_tag_list.append(f"{service_name}: [{service_aidl}]")
         i = i + 1
-    return accessible_services
+    
+    show_progress(i, total, "Done")
+    return accessible_service_tag_list
 
 def show_progress(now, total, msg):
     if total == 0:
@@ -51,11 +55,10 @@ def main(workspace):
     os.chdir(workspace)
     accessible_services = untrusted_app_service_accessible_anaylsis()
     with open('accessible_services.txt', 'w') as f:
-        for service in accessible_services:
-            f.write(service)
-            f.write('\n')
-        # For unknown reason no SELinux policy shows untrusted app can access window service, but it is accessible in real Android device.
-        f.write('window')
+        f.writelines(accessible_services)
+        # For unknown reason no SELinux policy shows untrusted app can access window service
+        # But it is accessible in real Android device.
+        f.write('window: [android.view.IWindowManager]')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
